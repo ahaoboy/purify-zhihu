37784e84968777fd03cf9c485e15d975f813387d
阿豪boy <504595380@qq.com>
2021-09-18 16:02:58 +0800

    fix: video ans

--- index.main.js
+++ index.main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         purify-zhihu
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.4
 // @description  屏蔽知乎首页视频
 // @author       You
 // @match        https://www.zhihu.com/
@@ -9,11 +9,23 @@
 // @grant        none
 // ==/UserScript==
 const q = ".Card.TopstoryItem.TopstoryItem > .Feed ";
-const inv = 1000 * 60 * 1; // 1 min
+const inv = 1000 * 2; // 2 s
+const hasVideo = (div) => {
+  const info = div.getAttribute("data-za-extra-module");
+  return info && JSON.parse(info)?.card?.has_video;
+};
+const check = (dom) => {
+  const div = dom.querySelector(".ContentItem");
+  if (hasVideo(dom) || hasVideo(div)) {
+    return true;
+  }
+  return false;
+};
+
 const f = () => {
   const list = Array.from(document.querySelectorAll(q));
   for (const item of list) {
-    if (item.children[0]?.classList?.contains?.("ContentItem")) {
+    if (check(item)) {
       item.parentElement?.remove?.();
     }
   }
