32f01de300b646515bcfedf41a96de11867b6585
阿豪boy <504595380@qq.com>
2021-09-20 14:43:27 +0800

    fix duration

--- index.main.js
+++ index.main.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         purify-zhihu
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  屏蔽知乎首页视频
 // @author       You
-// @match        https://www.zhihu.com/
+// @match        https://www.zhihu.com/*
 // @icon         https://pic4.zhimg.com/80/v2-88158afcff1e7f4b8b00a1ba81171b61_720w.png
 // @grant        none
 // ==/UserScript==
 const q = ".Card.TopstoryItem.TopstoryItem > .Feed ";
-const inv = 1000 * 2; // 2 s
+const inv = 1000 * 1; // 1 s
 const hasVideo = (div) => {
   const info = div.getAttribute("data-za-extra-module");
   return info && JSON.parse(info)?.card?.has_video;
