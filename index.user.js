6d602ffae1e106221fd0228d712cd091247df7dd
阿豪boy <504595380@qq.com>
2022-04-05 20:53:36 +0800

    feat: remove login

--- index.user.js
+++ index.user.js
@@ -1,21 +1,28 @@
 // ==UserScript==
 // @name         purify-zhihu
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  屏蔽知乎首页视频
 // @author       You
 // @match        https://www.zhihu.com/*
 // @icon         https://pic4.zhimg.com/80/v2-88158afcff1e7f4b8b00a1ba81171b61_720w.png
 // @grant        none
 
+//0. 更新地址: https://purify-zhihu.vercel.app/index.user.js
 //1. 去除非登录下的弹框
 //2. 去除视频推送
+//3. 去除登录框和视频回答
 // ==/UserScript==
 const inv = 1000 * 1; // 1 s
 
 // 去除视频
 const f1 = () => {
   const hasVideo = (div) => {
+    // 包含视频回答的, svg封面, 可以直接点开播放
+    if (div.querySelector(".RichContent-cover-play")) {
+      return true;
+    }
+    // 根据类型信息判断
     const info = div.getAttribute("data-za-extra-module");
     return info && JSON.parse(info)?.card?.has_video;
   };
@@ -41,17 +48,22 @@ const f2 = () => {
   const q = ".Modal-wrapper";
   const dom = document.querySelector(q);
   if (!dom) return;
+  // 不是登录框
+  if (!dom.querySelector(".signFlowModal-container")) return;
   const s = ".Button.Modal-closeButton.Button--plain";
   const b = document.querySelector(s);
-  // b?.click?.();
+  // 只去除登录框, 不去除评论等弹窗
+  b?.click?.();
 };
 
 // 去除广告
 const f3 = () => {
-  const adList = Array.from(document.querySelectorAll(".TopstoryItem--advertCard"));
-  for(const item of adList){
-      // item.parentElement?.remove?.();
-      item.innerHTML = ""
+  const adList = Array.from(
+    document.querySelectorAll(".TopstoryItem--advertCard")
+  );
+  for (const item of adList) {
+    // item.parentElement?.remove?.();
+    item.innerHTML = "";
   }
 };
 const run = () => {
